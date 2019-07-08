cf7dcde2c98a0c8d7190032fe3ec2037d7cbe61f
xiongchao <xiongchao@git.com>
2019-07-08 08:38:27 +0800

    第一次提交基础文件信息

---
+++ demo_app/src/components/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="login">
+    <h1>this is login page</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){return {}},
+}
+</script>
---
+++ demo_app/src/views/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Index">
+    <h1>this is Index page</h1>
+      <button class="login">登录</button>
+    <login></login>
+  </div>
+</template>
+
+<script>
+import Login from '../components/Login.vue'
+export default {
+  components:{"login":Login},
+  data(){ return {}},
+  mounted(){
+
+  
+
+  },
+}
+$(function(){
+    $(".login").click(function(){
+       alert("点我试试看")
+       console.log(111)
+     })
+})
+</script>
